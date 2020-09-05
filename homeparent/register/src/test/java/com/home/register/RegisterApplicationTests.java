452892ed47c216460dbc0f614762b68e44ec264b
GTsung <xc_goo@163.com>
2020-09-06 02:09:02 +0800

    first

---
+++ homeparent/register/src/test/java/com/home/register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
